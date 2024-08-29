f87cac3218c7c76d80120913e055c0d32ed40561
João Antônio A. D. S. S. <joao.antonio.amorim.8@gmail.com>
2024-08-28 23:57:40 -0300

    Adicionado efeito no main

---
+++ Main-projeto/vinqua.js
@@ -0,0 +1,9 @@
+    window.onload = function() {
+
+        setTimeout(function(){
+            document.querySelector('main#container-inicial').classList.add('mostrar');
+            /* Após 500 milissegundos (500 milissegundos), a classe "mostrar" é adicionada ao elemento,
+           ativando a transição de opacidade e fazendo o elemento aparecer lentamente. */
+        }, 500);
+
+    }
